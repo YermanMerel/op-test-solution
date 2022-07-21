52b83d3151c1bf16a8271500fc9b29344ac73777
Yerman Merel <yermanmerel@gmail.com>
2022-07-21 01:12:02 -0500

    Completed test 2

---
+++ test2/index.js
@@ -0,0 +1,3 @@
+let mySelft = ['Yerman', 19, true, 2/5/2000, {title: 'eloquentJS', autor: 'Marijin Haverbeke', year: 2008, url: 'https://eloquentjs-es.thedojo.mx/00_intro.html'}];
+
+console.log('mySelft', mySelft);
